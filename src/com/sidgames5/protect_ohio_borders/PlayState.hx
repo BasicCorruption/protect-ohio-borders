462c940ebb2e1140a5d040efd6b2817c4e051900
Sid <saytleader@outlook.com>
2023-03-16 12:47:50 -0400

    Create PlayState.hx

---
+++ src/com/sidgames5/protect_ohio_borders/PlayState.hx
@@ -0,0 +1,16 @@
+package com.sidgames5.protect_ohio_borders;
+
+import flixel.FlxState;
+
+class PlayState extends FlxState
+{
+	override public function create()
+	{
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
