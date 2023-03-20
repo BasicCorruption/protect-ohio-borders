912c547a7879edea649a0b932701a88ebd1df615
Sid <saytleader@outlook.com>
2023-03-20 13:05:14 -0400

    Update MenuState.hx

--- src/com/sidgames5/protect_ohio_borders/MenuState.hx
+++ src/com/sidgames5/protect_ohio_borders/MenuState.hx
@@ -4,23 +4,34 @@ import flixel.FlxState;
 
 class MenuState extends Scene
 {
-	private static var Objects:Array<FlxBasic> = [];
-	
 	private static var title:FlxText;
+	private static final titleSize:Int = 48;
+	private static var playButton:FlxSprite;
 	
 	override public function create()
 	{
 		super.create();
 		
 		title = new FlxText(0, 0, 0, "Protect Ohio Borders");
-		title.setFormat(null, 32, FlxColor.WHITE, CENTER);
-		Objects.push(title);
+		title.setFormat(null, titleSize, FlxColor.WHITE, CENTER);
+		title.screenCenter(X);
 		
-		drawObjects();
+		// TODO: Make play button sprite
+		playButton = new FlxSprite(Assets.play_button__png);
+		playButton.screenCenter(XY);
+		
+		add(title);
+		add(playButton);
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+		
+		if (title.size <= titleSize) {
+			title.size++;
+		} else if (title.size >= (1.5*titleSize)) {
+			title.size--;
+		}
 	}
 }
