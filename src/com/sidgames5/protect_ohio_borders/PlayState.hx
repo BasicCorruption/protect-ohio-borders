dda4e95d9ff1e407a6761911dbc6f2e77189fa80
Sid <saytleader@outlook.com>
2023-03-20 13:09:42 -0400

    Create PlayState.hx

---
+++ src/com/sidgames5/protect_ohio_borders/PlayState.hx
@@ -0,0 +1,22 @@
+package com.sidgames5.protect_ohio_borders;
+
+import flixel.FlxState;
+
+class PlayState extends FlxState
+{
+	private static var border:FlxSprite;
+	private static var player:FlxSprite;
+	private static var weapon:Weapon;
+	
+	//private static var escapees:Array<;
+	
+	override public function create()
+	{
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
