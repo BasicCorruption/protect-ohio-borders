7ba11118932079ff01f7d782dd42620d69fcb897
Sid <saytleader@outlook.com>
2023-03-20 12:56:11 -0400

    Update MenuState.hx

--- src/com/sidgames5/protect_ohio_borders/MenuState.hx
+++ src/com/sidgames5/protect_ohio_borders/MenuState.hx
@@ -2,13 +2,21 @@ package com.sidgames5.protect_ohio_borders;
 
 import flixel.FlxState;
 
-class MenuState extends FlxState
+class MenuState extends Scene
 {
+	private static var Objects:Array<FlxBasic> = [];
+	
 	private static var title:FlxText;
 	
 	override public function create()
 	{
 		super.create();
+		
+		title = new FlxText(0, 0, 0, "Protect Ohio Borders");
+		title.setFormat(null, 32, FlxColor.WHITE, CENTER);
+		Objects.push(title);
+		
+		drawObjects();
 	}
 
 	override public function update(elapsed:Float)
