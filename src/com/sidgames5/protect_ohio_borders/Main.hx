d87dbc9fa7658e381b0954357a427591d3d95540
Sid <saytleader@outlook.com>
2023-03-20 13:05:30 -0400

    Update Main.hx

--- src/com/sidgames5/protect_ohio_borders/Main.hx
+++ src/com/sidgames5/protect_ohio_borders/Main.hx
@@ -3,6 +3,6 @@ package com.sidgames5.protect_ohio_borders;
 class Main extends Sprite {
 	public function new() {
 		super();
-		Lib.curent.addChild(new FlxGame(0, 0, PlayState, 1, 60, 60, true, false));
+		Lib.curent.addChild(new FlxGame(0, 0, MenuState, 1, 60, 60, true, false));
 	}
 }
